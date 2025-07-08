a9bc61c0e94d2010836d047e5bb64c8276282e24
Matheus Schneider <matheusschneider503@gmail.com>
2025-07-08 13:46:38 -0300

    CRUD da Classe Categoria Implementado

---
+++ src/Categoria/entities/categoria.entity.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Produto } from '../../produto/entities/produto.entity';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 50, nullable: false })
+  categoria: string;
+
+  @OneToMany(() => Produto, (produto) => product.categoria)
+  produto: Produto[];
+}
